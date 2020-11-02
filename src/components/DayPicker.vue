12d2f3ac2505cec2dcd44fbb29e1372fc5e9ce16
Anuntachai Naewsuwan <toey.anuntachai@gmail.com>
2020-11-02 15:37:58 +0700

    add prototype components

---
+++ src/components/DayPicker.vue
@@ -0,0 +1,41 @@
+<template>
+  <picker-scroll :item-list="days" @change="onChange" />
+</template>
+
+<script>
+import PickerScroll from './PickerScroll.vue';
+
+export default {
+  name: 'DayPicker',
+  components: {
+    PickerScroll
+  },
+  props: {
+    value: {
+      type: Number,
+      default: null
+    }
+  },
+  data() {
+    return {
+      days: []
+    };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      const days = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 1; i <= 31; i++) {
+        days.push({ value: i, name: i, selected: this.value === i });
+      }
+      this.days = days;
+    },
+    onChange(value) {
+      this.$emit('change', value);
+    }
+  }
+};
+</script>
--- src/vue-datepicker-scroll.vue
+++ src/vue-datepicker-scroll.vue
@@ -1,77 +1,294 @@
-<script>
-export default {
-  name: 'VueDatepickerScroll', // vue component name
-  data() {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this;
-      if (!message.action) return 'initialized';
-      return `${message?.action} ${message.amount ?? ''}`.trim();
-    },
-  },
-  methods: {
-    increment(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset() {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
-    },
-  },
-};
-</script>
-
 <template>
-  <div class="vue-datepicker-scroll">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
+  <div>
+    <input
+        id="birthday"
+        type="text"
+        :class="computedInputClass"
+        inputmode="numeric"
+        :placeholder="placeHolder"
+        data-testid="input-picker"
+        autocomplete="off"
+        readonly="readonly"
+        :disabled="disabled"
+        :value="selectedDate"
+        @click="togglePicker"
+    />
+    <transition name="modal" data-testid="transition">
+      <div v-if="isOpenPicker" class="modal-mask" data-testid="picker-modal">
+        <div class="modal-wrapper">
+          <div class="modal-container p-0">
+            <div class="modal-body p-0 m-0">
+              <day-picker :value="day" data-testid="day-picker" @change="selectDay" />
+              <month-picker
+                  :value="month"
+                  data-testid="month-picker"
+                  class="month-picker"
+                  @change="selectMonth"
+              />
+              <year-picker :value="year" data-testid="year-picker" @change="selectYear" />
+            </div>
+
+            <div class="modal-footer d-flex p-0">
+              <slot name="footer">
+                <button
+                    type="button"
+                    class="btn btn-block border-0 m-0 p-0 rounded-0"
+                    data-testid="confirm"
+                    @click="onConfirm"
+                >
+                  <i class="far fa-check"></i>
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-block border-0 m-0 p-0 rounded-0"
+                    data-testid="cancel"
+                    @click="onCancel"
+                >
+                  <i class="far fa-times"></i>
+                </button>
+              </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
+<script>
+import YearPicker from "@/components/YearPicker";
+import MonthPicker from "@/components/MonthPicker";
+import DayPicker from "@/components/DayPicker";
+
+export default {
+  name: 'VueDatepickerScroll',
+  components: {
+    YearPicker,
+    MonthPicker,
+    DayPicker
+  },
+  props: {
+    inputClass: {
+      type: [String, Object],
+      default: ''
+    },
+    value: {
+      type: [Date, String],
+      default: null
+    },
+    placeHolder: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      selectedDate: this.value,
+      isOpenPicker: false,
+      day: 1,
+      month: 0,
+      year: 2483
+    };
+  },
+  computed: {
+    computedInputClass() {
+      if (typeof this.inputClass === 'string') {
+        return [this.inputClass, 'form-control'].join(' ');
+      }
+      return { 'form-control': true, ...this.inputClass };
+    }
+  },
+  created() {
+    if (this.value && typeof this.value === 'string') {
+      const [day, month, year] = this.value.split('/');
+      this.day = Number(day.replace('^0+', ''));
+      this.month = Number(month.replace('^0+', '')) - 1;
+      this.year = Number(year);
+    }
+  },
+  methods: {
+    onInput($event) {
+      this.selectedDate = $event.target.value;
+      this.$emit('input', $event.target.value);
+      this.$emit('change', $event.target.value);
+    },
+    togglePicker() {
+      this.isOpenPicker = !this.isOpenPicker;
+      this.handleBodyScroll();
+    },
+    onConfirm() {
+      this.parseDate();
+      this.$emit('input', this.selectedDate);
+      this.$emit('change', this.selectedDate);
+      this.closePicker();
+    },
+    onCancel() {
+      this.closePicker();
+      this.$emit('close');
+    },
+    closePicker() {
+      this.isOpenPicker = false;
+      this.handleBodyScroll();
+    },
+    selectMonth(month) {
+      this.month = month;
+    },
+    selectDay(day) {
+      this.day = day;
+    },
+    selectYear(year) {
+      this.year = year;
+    },
+    parseDate() {
+      const date = new Date(this.year, this.month, this.day);
+      const day = date
+          .getDate()
+          .toString()
+          .padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      this.selectedDate = `${day}/${month}/${date.getFullYear()}`;
+      this.$emit('change', date);
+    },
+    handleBodyScroll() {
+      document.body.classList.toggle('overflow-hidden');
+    }
+  }
+};
+</script>
+
 <style scoped>
-  .vue-datepicker-scroll {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: auto;
+  height: 100vh;
+  @media (min-width: 992px) {
+    width: 744px;
+    left: calc(280px / 2);
   }
-  .vue-datepicker-scroll p {
-    margin: 0 0 1em;
+}
+
+.modal-container {
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  width: 280px;
+  @media (min-width: 375px) {
+    width: 345px;
   }
+  ::v-deep {
+    .vscroller {
+      padding: 0;
+    }
+    .vscroller__body {
+      &::before {
+        background-color: #a2acb3;
+        height: 40px;
+      }
+      &::after {
+        content: none;
+      }
+      li.vselected {
+        font-weight: normal;
+      }
+      li {
+        cursor: grab;
+      }
+    }
+    .btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 47px;
+      i {
+        color: #767676;
+        font-size: 22px;
+        font-weight: 700;
+      }
+      &:focus {
+        box-shadow: none;
+      }
+    }
+  }
+  .month-picker {
+    border-left: 2px solid #d4dde2;
+    border-right: 2px solid #d4dde2;
+  }
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+  display: flex;
+  background-color: #f2f2f2;
+  max-height: 365px;
+}
+.modal-footer {
+  background-color: #ebebeb;
+  height: 47px;
+  border-radius: 0;
+  border-top: 1px solid #d4dde2;
+  position: relative;
+  &::before {
+    content: '';
+    display: flex;
+    height: 100%;
+    position: absolute;
+    margin: auto;
+    left: 0;
+    right: 0;
+    width: 1px;
+    background-color: #d4dde2;
+  }
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
 </style>
