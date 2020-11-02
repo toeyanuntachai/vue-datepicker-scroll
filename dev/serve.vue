1a87e0fb5c0a081072e849c633644bc0dd4d44b5
Anuntachai Naewsuwan <toey.anuntachai@gmail.com>
2020-11-02 12:08:08 +0700

    init project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueDatepickerScroll from '@/vue-datepicker-scroll.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueDatepickerScroll
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-datepicker-scroll />
+  </div>
+</template>
